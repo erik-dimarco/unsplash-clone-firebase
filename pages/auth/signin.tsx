76283c469e7c06a5c0bb13411906526fa188e52b
Erik DiMarco <erik.dimarco@dialexa.com>
2022-08-05 14:47:23 -0500

    Convert modals to popovers from headless ui.

--- pages/auth/signin.tsx
+++ pages/auth/signin.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { getProviders, signIn } from "next-auth/react";
 import { useRouter } from "next/router";
+import Image from "next/image";
 
 type SignInProps = {
   providers: any;
@@ -12,10 +13,27 @@ const SignIn = ({ providers }: SignInProps) => {
   return (
     <>
       {Object.values(providers).map((provider: any) => (
-        <div key={provider.name}>
-          <button onClick={() => signIn(provider.id)}>
-            Sign in with {provider.name}
-          </button>
+        <div className="flex flex-col items-center justify-center min-h-screen">
+          <div className="flex flex-col items-center justify-center">
+            <div
+              onClick={() => router.push("/")}
+              className="relative h-20 w-20 flex-shrink-0 cursor-pointer mb-6"
+            >
+              <Image objectFit="contain" src="/logo.svg" layout="fill" />
+            </div>
+            <h1 className="text-3xl font-bold mb-6">Login</h1>
+            <p className="text-sm mb-6">Welcome back.</p>
+          </div>
+          <div
+            onClick={() => signIn(provider.id, { callbackUrl: "/" })}
+            key={provider.name}
+            className="flex  pr-2 bg-[#3F7EE8] rounded-md p-1 shadow-sm items-center justify-center"
+          >
+            <div className="bg-white p-2 h-full mr-2 items-center justify-center cursor-pointer">
+              <img src="/Google__G__Logo.svg" />
+            </div>
+            <button className="text-white">Sign in with {provider.name}</button>
+          </div>
         </div>
       ))}
     </>
